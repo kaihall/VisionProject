3271e9d45f0c9566647070faac6105a5048ca36f
kaihall <49812013+kaihall@users.noreply.github.com>
2019-11-24 15:32:21 -0500

    Combined ScoutBotStrategy1 changes
    Combined code from previous commits. Extended dead end functionality to mark a piece as a dead end if all or all but one of its neighbors are dead ends.

--- ScoutBotStrategy1.java
+++ ScoutBotStrategy1.java
@@ -4,57 +4,73 @@ import java.util.Random;
 
 public class ScoutBotStrategy1 extends BotStrategy
 {
-	
+    Random rand;
+    
     public ScoutBotStrategy1() {
-		// creates a new instance of random
-		Random rand = new Random();
+        // creates a new instance of random
+        rand = new Random();
     }
     
     public Command nextMove(Robot bot, List<Location> cur_vision, Map map) {
-		//gets a list of directions
-		List<DirType> possibleDirections = cur_vision.getDirections();
-		
-		// gets the bots current location in the maze
-		int xLoc = cur_vision.getX();
-		int yLoc = cur_vision.getY();
-		
-		// checks to see if theres only one possible direction to move in
-		// if there is it'll mark that location as a dead end
-		if (possibleDirections.size == 1){
-			map.setLabel(xLoc, yLoc, 2);
-		}
-		
-		// if the direction is both possible AND unexplored, then the bot will go that way
-		// the bot checks three away from the current posistion in each direction
-		if (possibleDirections.contains(DirType.North) && map.getLabel(xLoc , yLoc + 3) == 0) {
-			return new CommandMove(bot,DirType.North);
-		}
-		else if (possibleDirections.contains(DirType.East)&& map.getLabel(xLoc + 3, yLoc) == 0) {
-			return new CommandMove(bot,DirType.East);
-		}
-		else if (possibleDirections.contains(DirType.South) && map.getLabel(xLoc, yLoc - 3) == 0) {
-			return new CommandMove(bot,DirType.South);
-		}
-		else if (possibleDirections.contains(DirType.West) && map.getLabel(xLoc - 3, yLoc) == 0) {
-			return new CommandMove(bot,DirType.West);
-		}
-		// if all of the directions are explored then the bot picks a random direction and moves that way
-		else{
-			// direction is based on the list of possible directions
-			int randomIndex= rand.nextInt(possibleDirections.size);
-			String direction = possibleDirections.get(randomIndex);
-			if (direction = DirType.North) {
-				return new CommandMove(bot, DirType.North);
-			}
-			if (direction = DirType.East) {
-				return new CommandMove(bot, DirType.East);
-			}
-			if (direction = DirType.South) {
-				return new CommandMove(bot, DirType.South);
-			}
-			if (direction = DirType.West) {
-				return new CommandMove(bot, DirType.West);
-			}
-		}
+        //gets the bot's current location
+        Location cur_loc = map.getBotLocation(bot);
+        
+        //gets a list of directions
+        List<DirType> possibleDirections = cur_loc.getDirections();
+        
+        // gets the bots current location in the maze
+        int xLoc = cur_loc.getX();
+        int yLoc = cur_loc.getY();
+        
+        // checks to see if theres only one possible direction to move in
+	// if there is it'll mark that location as a dead end
+	if (deadEnd(map,cur_loc,xLoc,yLoc)){
+	    map.setLabel(xLoc, yLoc, Map.DEAD_END);
+	}
+        
+        // if the direction is both possible AND unexplored, then the bot will go that way
+        // the bot checks three away from the current posistion in each direction
+        if (possibleDirections.contains(DirType.North) && map.getLabel(xLoc , yLoc + 3) == Map.UNSCANNED) {
+            return new CommandMove(bot,DirType.North);
+        }
+        else if (possibleDirections.contains(DirType.East)&& map.getLabel(xLoc + 3, yLoc) == Map.UNSCANNED) {
+            return new CommandMove(bot,DirType.East);
+        }
+        else if (possibleDirections.contains(DirType.South) && map.getLabel(xLoc, yLoc - 3) == Map.UNSCANNED) {
+            return new CommandMove(bot,DirType.South);
+        }
+        else if (possibleDirections.contains(DirType.West) && map.getLabel(xLoc - 3, yLoc) == Map.UNSCANNED) {
+            return new CommandMove(bot,DirType.West);
+        }
+        // if all of the directions are explored then the bot picks a random direction and moves that way
+        else{
+            // direction is based on the list of possible directions
+            int randomIndex = rand.nextInt(possibleDirections.size());
+            DirType direction = possibleDirections.get(randomIndex);
+            return new CommandMove(bot,direction);
+        }
+    }
+    
+    private boolean deadEnd(Map map, Location curLoc, int xLoc, int yLoc) {
+        List<DirType> dirxns = curLoc.getDirections();
+        
+        if (dirxns.size() == 1) return true;
+        
+        else {
+            int deadEnds = 0;
+            for (DirType check : dirxns) {
+                if (check == DirType.North && yLoc > 0 && map.getLabel(xLoc,yLoc-1) == Map.DEAD_END)
+                    deadEnds++;
+                if (check == DirType.South && yLoc < map.getMaxY() && map.getLabel(xLoc,yLoc+1) == Map.DEAD_END)
+                    deadEnds++;
+                if (check == DirType.East && xLoc > 0 && map.getLabel(xLoc-1,yLoc) == Map.DEAD_END)
+                    deadEnds++;
+                if (check == DirType.West && xLoc < map.getMaxX() && map.getLabel(xLoc+1,yLoc) == Map.DEAD_END)
+                    deadEnds++;
+            }
+            
+            //if all directions are dead ends or all but one are, this location is also a dead end
+            return deadEnds >= dirxns.size()-1;
+        }
     }
 }
