ff6bcf7d62975682ccf30abe0dea981c90f2724f
addiefluegge <32827316+addiefluegge@users.noreply.github.com>
2019-11-24 00:23:00 -0500

    changed directions to dirtypes

--- ScoutBotStrategy1.java
+++ ScoutBotStrategy1.java
@@ -6,30 +6,30 @@ public class ScoutBotStrategy1 extends BotStrategy
 {
 	
     public ScoutBotStrategy1() {
-		// sets up the random direction
+		// creates a new instance of random
 		Random rand = new Random();
     }
     
     public Command nextMove(Robot bot, List<Location> cur_vision, Map map) {
-    		//gets a list of directions
-		List possibleDirections = cur_vision.getDirections();
+		//gets a list of directions
+		List<DirType> possibleDirections = cur_vision.getDirections();
 		
-		// gets the bots current location
+		// gets the bots current location in the maze
 		int xLoc = cur_vision.getX();
 		int yLoc = cur_vision.getY();
 		
 		// if the direction is both possible AND unexplored, then the bot will go that way
 		// the bot checks three away from the current posistion in each direction
-		if (possibleDirections.contains("North") && Map.getLabel(xLoc , yLoc + 3) == 0) {
+		if (possibleDirections.contains(DirType.North) && Map.getLabel(xLoc , yLoc + 3) == 0) {
 			return new CommandMove(bot,DirType.North);
 		}
-		else if (possibleDirections.contains("East")&& Map.getLabel(xLoc + 3, yLoc) == 0) {
+		else if (possibleDirections.contains(DirType.East)&& Map.getLabel(xLoc + 3, yLoc) == 0) {
 			return new CommandMove(bot,DirType.East);
 		}
-		else if (possibleDirections.contains("South") && Map.getLabel(xLoc, yLoc - 3) == 0) {
+		else if (possibleDirections.contains(DirType.South) && Map.getLabel(xLoc, yLoc - 3) == 0) {
 			return new CommandMove(bot,DirType.South);
 		}
-		else if (possibleDirections.contains("West") && Map.getLabel(xLoc - 3, yLoc) == 0) {
+		else if (possibleDirections.contains(DirType.West) && Map.getLabel(xLoc - 3, yLoc) == 0) {
 			return new CommandMove(bot,DirType.West);
 		}
 		// if all of the directions are explored then the bot picks a random direction and moves that way
@@ -37,16 +37,16 @@ public class ScoutBotStrategy1 extends BotStrategy
 			// direction is based on the list of possible directions
 			int randomIndex= rand.nextInt(possibleDirections.size);
 			String direction = possibleDirections.get(randomIndex);
-			if (direction = "North") {
+			if (direction = DirType.North) {
 				return new CommandMove(bot, DirType.North);
 			}
-			if (direction = "East") {
+			if (direction = DirType.East) {
 				return new CommandMove(bot, DirType.East);
 			}
-			if (direction = "South") {
+			if (direction = DirType.South) {
 				return new CommandMove(bot, DirType.South);
 			}
-			if (direction = "West") {
+			if (direction = DirType.West) {
 				return new CommandMove(bot, DirType.West);
 			}
 		}
